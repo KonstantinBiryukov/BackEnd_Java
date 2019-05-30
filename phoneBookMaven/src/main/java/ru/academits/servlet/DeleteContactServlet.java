5fa120d811a8f12e261ac0a19ee4d3b0dd68c20c
Konstantin Biriukov <kostya-leon@mail.ru>
2019-05-30 23:49:58 +0700

    initial commit

---
+++ phoneBookMaven/src/main/java/ru/academits/servlet/DeleteContactServlet.java
@@ -0,0 +1,32 @@
+package ru.academits.servlet;
+
+import ru.academits.PhoneBook;
+import ru.academits.coverter.ContactConverter;
+import ru.academits.model.Contact;
+import ru.academits.service.ContactService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.stream.Collectors;
+
+
+public class DeleteContactServlet extends HttpServlet {
+    private static final long serialVersionUID = -4554423888294460746L;
+    private ContactService phoneBookService = PhoneBook.phoneBookService;
+    private ContactConverter contactConverter = PhoneBook.contactConverter;
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            String contactJson = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+            Contact contact = contactConverter.convertFormJson(contactJson);
+            phoneBookService.deleteContact(contact.getNumber());
+//            resp.setStatus(500);
+        } catch (Exception e) {
+            System.out.println("error in DeleteAllContactsServlet");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ phoneBookMaven/src/main/java/ru/academits/servlet/SearchContactsServlet.java
@@ -0,0 +1,32 @@
+package ru.academits.servlet;
+
+import ru.academits.PhoneBook;
+import ru.academits.coverter.ContactConverter;
+import ru.academits.model.Contact;
+import ru.academits.service.ContactService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class SearchContactsServlet extends HttpServlet {
+    private static final long serialVersionUID = -6616095261016617030L;
+    private ContactService phoneBookService = PhoneBook.phoneBookService;
+    private ContactConverter contactConverter = PhoneBook.contactConverter;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            System.out.println(req);
+            List<Contact> contactList = phoneBookService.getAllContacts();
+            String contactListJson = contactConverter.convertToJson(contactList);
+            resp.getOutputStream().write(contactListJson.getBytes(Charset.forName("UTF-8")));
+            resp.getOutputStream().flush();
+            resp.getOutputStream().close();
+        } catch (Exception e) {
+            System.out.println("error in GetAllContactsServlet GET: ");
+            e.printStackTrace();
+        }
+    }
+}
