1c46963b0fb94f3cc870c6facee3395abbc42fbe
Konstantin Biriukov <kostya-leon@mail.ru>
2019-07-03 18:19:47 +0700

    Foreig key refractoring

--- PhoneBookHibernate/src/main/java/ru/academits/service/CallService.java
+++ PhoneBookHibernate/src/main/java/ru/academits/service/CallService.java
@@ -3,20 +3,28 @@ package ru.academits.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.academits.dao.CallDao;
+import ru.academits.dao.ContactDao;
 import ru.academits.model.Call;
+import ru.academits.model.Contact;
 
 import java.util.List;
 
 @Service
 public class CallService {
     private final CallDao callDao;
+    private final ContactDao contactDao;
 
     @Autowired
-    public CallService(CallDao callDao) {
+    public CallService(CallDao callDao, ContactDao contactDao) {
         this.callDao = callDao;
+        this.contactDao = contactDao;
     }
 
     public void addCall(Call call) {
+        // setContact to assign foreign key in Call entity
+        Contact contact = contactDao.findByPhone(call.getCallNumber()).get(0);
+        call.setContact(contact);
+
         callDao.save(call);
     }
 
--- PhoneBookHibernate/src/main/java/ru/academits/service/CallToCallEntityConvert.java
+++ PhoneBookHibernate/src/main/java/ru/academits/service/CallToCallEntityConvert.java
@@ -1,11 +1,8 @@
 package ru.academits.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.academits.dao.ContactDao;
 import ru.academits.dto.CallDto;
 import ru.academits.model.Call;
-import ru.academits.model.Contact;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +10,11 @@ import java.util.List;
 @SuppressWarnings("ALL")
 @Service
 public class CallToCallEntityConvert {
-
-    @Autowired
-    ContactDao contactDao;
-
     public Call toEntity(CallDto callDto) {
-
         Call call = new Call();
         call.setId(callDto.getId());
         call.setCallDate(callDto.getCallDate());
         call.setCallNumber(callDto.getCallNumber());
-
-        Contact contact = contactDao.findByPhone(callDto.getCallNumber()).get(0);
-        call.setContact(contact);
         return call;
     }
 
